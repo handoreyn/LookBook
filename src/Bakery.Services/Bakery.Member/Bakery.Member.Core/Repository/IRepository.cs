f212280521f92e7d8f2a8db2c7db8931bb8d5b67
handoreyn <fatihgencaslan@yahoo.com>
2022-03-12 23:34:15 +0300

    repository defined.

---
+++ src/Bakery.Services/Bakery.Member/Bakery.Member.Core/Repository/IRepository.cs
@@ -0,0 +1,25 @@
+using MongoDB.Driver;
+
+public interface IRepository<TEntity> where TEntity : class
+{
+    TEntity Find();
+    Task<TEntity> FindAsync();
+    TEntity Find(FilterDefinition<TEntity> filter);
+    Task<TEntity> FindAsync(FilterDefinition<TEntity> filter);
+    public IEnumerable<TEntity> FindMany(FilterDefinition<TEntity> filter, int limit = 10, int page = 0);
+    public Task<List<TEntity>> FindManyAsync(FilterDefinition<TEntity> filter, int limit = 10, int page = 0);
+    public List<TEntity> FindMany(FilterDefinition<TEntity> filter, SortDefinition<TEntity> sort, int limit = 10, int page = 0);
+    public Task<List<TEntity>> FindManyAsync(FilterDefinition<TEntity> filter, SortDefinition<TEntity> sort, int limit = 10, int page = 0);
+
+    TEntity Create(TEntity T);
+    Task<TEntity> CreateAsync(TEntity T);
+    void Update(FilterDefinition<TEntity> filter, UpdateDefinition<TEntity> update, UpdateOptions options);
+    Task UpdateAsync(FilterDefinition<TEntity> filter, UpdateDefinition<TEntity> update, UpdateOptions options);
+    void UpdateMany(FilterDefinition<TEntity> filter, UpdateDefinition<TEntity> update, UpdateOptions options);
+    Task UpdateManyAsync(FilterDefinition<TEntity> filter, UpdateDefinition<TEntity> update, UpdateOptions options);
+
+    void Delete(FilterDefinition<TEntity> filter);
+    Task DelteAsync(FilterDefinition<TEntity> filter);
+    void DeleteMany(FilterDefinition<TEntity> filter);
+    Task DeleteManyAsync(FilterDefinition<TEntity> filter);
+}
