80cd7a6dd9e71dc220f5c5b4df5198de8073ec69
handoreyn <fatihgencaslan@yahoo.com>
2022-03-27 02:21:32 +0300

    email template entity has been created.

---
+++ src/Bakery.Services/Bakery.Email/Bakery.Email.Core/Entities/EmailTemplate.cs
@@ -0,0 +1,20 @@
+using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace Bakery.Email.Core.Entities;
+
+public class EmailTemplate
+{
+    [BsonId]
+    public ObjectId Id { get; set; }
+    [BsonElement("country")]
+    public string Country { get; set; }
+    [BsonElement("language")]
+    public string Language { get; set; }
+    [BsonElement("title")]
+    public string Title { get; set; }
+    [BsonElement("content")]
+    public string Content { get; set; }
+    [BsonElement("parameters")]
+    public List<string> Parameters { get; set; } = new();
+}
